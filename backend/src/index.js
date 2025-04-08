d4d539af3a04aef11310cc1f21accafd793e33a6
wekesaoliver <oliverwekesa361@gmail.com>
2025-04-09 00:22:15 +0300

    update mobile responsiveness

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     });
 }
 
