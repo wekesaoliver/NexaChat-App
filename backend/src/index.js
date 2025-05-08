28934949603bed1573d318ebb8a45450853c7f03
Oliver Wekesa <111235034+wekesaoliver@users.noreply.github.com>
2025-05-08 14:09:04 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const port = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 app.use(
     cors({
